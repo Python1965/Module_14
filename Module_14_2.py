643f73ef621408c4a87850ead4dd2aeb665d6b3b
Python1965 <python65@rambler.ru>
2024-12-09 06:25:17 +0300

    Add files via upload

--- Module_14_2.py
+++ Module_14_2.py
@@ -19,6 +19,33 @@ import sqlite3
 connection = sqlite3.connect('not_telegram.db')
 cursor = connection.cursor()
 
+# cursor.execute('''
+# CREATE TABLE IF NOT EXISTS Users(
+# id INTEGER PRIMARY KEY,
+# username TEXT NOT NULL,
+# email TEXT NOT NULL,
+# age INTEGER,
+# balance INTEGER NOT NULL
+# )
+# ''')
+#
+# # ----------------- Добавление информации в базу данных ------------
+# for n in range(1,11):
+#     cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)', (f'User{n}', f'example{n}@gmail.com', f'{n*10}', '1000'))
+#
+# # ----------------- Изменение данных в базе данных -----------------
+# for i in range(1, 11, 2):
+#     cursor.execute('UPDATE Users SET balance = ? WHERE username = ?', (500 , f'User{i}'))
+#
+# # ----------------- Удаление данных из БД -------------------------
+# for i in range(1, 11, 3):
+#     cursor.execute('DELETE FROM Users WHERE username = ?', (f'User{i}', ))
+#
+# # ----------------- Выборка данных из БД ------------------------
+# cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != ?', (60,))
+# users = cursor.fetchall()
+# for user in users:
+#     print(f'Имя:{user[0]} | Почта:{user[1]} | Возраст:{user[2]} | Баланс:{user[3]}')
 
 # ---------------- Удаление строки по условиюиз БД --------------
 cursor.execute('DELETE FROM Users WHERE id = ?', (6,))
